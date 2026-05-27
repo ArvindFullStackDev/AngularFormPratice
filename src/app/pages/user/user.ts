476c33dbfb0eaac350fd8b835d9422191cb5aacc
ArvindFullStackDev <arvindfullstackdev@gmail.com>
2026-05-27 18:46:43 +0530

    User Update Form

--- src/app/pages/user/user.ts
+++ src/app/pages/user/user.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { UserService } from '../../core/services/user-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule ],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
 
-  constructor(private userService : UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   UserFormDetails = new FormGroup({
     Id: new FormControl(),
@@ -20,8 +21,14 @@ export class User {
     address: new FormControl()
   })
 
-    onSubmit(){
-        this.userService.addUser(this.UserFormDetails.getRawValue());
+  onSubmit() {
+    const user = this.userService.getUser();
+    const lastUser = user[user.length - 1];
+    const newId = lastUser ? lastUser.Id + 1 : 1;
+    const formData = this.UserFormDetails.getRawValue();
+    formData.Id = newId;
+    this.userService.addUser(formData);
+    this.router.navigate(['/users']);
     }
 
 
